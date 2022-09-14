1d3ca83b5d5f4ff2a2cf73646730b874d81ad28b
HBetaRoyal <84717201+HBetaRoyal@users.noreply.github.com>
2022-09-14 10:48:24 +0530

    Add files via upload

--- DocxScanner/app_ad_code_id.php
+++ DocxScanner/app_ad_code_id.php
@@ -2,8 +2,8 @@
 	"ad_id":
 		[
 			{
-			"g_ad":"1",
-			"fb_ad":"0",
+			"g_ad":"0",
+			"fb_ad":"1",
 			"adpub_id":"pub-2602596340797585",
 			"adapp_id":"ca-app-pub-2602596340797585~6072907965",
 			"adappopen_id":"ca-app-pub-2602596340797585/4568254602",
@@ -11,11 +11,11 @@
 			"adbanner_rect_id":"ca-app-pub-2602596340797585/2872029552",
 			"adinterstial_id":"ca-app-pub-2602596340797585/6619702879",
 			"adnativebanner_id":"ca-app-pub-2602596340797585/2680457866",
-			"adfbbanner_id":"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-			"adfbinterstial_id":"YOUR_PLACEMENT_ID",
-			"adfbnative_id":"YOUR_PLACEMENT_ID",
-			"adfbnative_banner_id":"YOUR_PLACEMENT_ID",		
-			"adfbrectangle_banner_id":"YOUR_PLACEMENT_ID"
+			"adfbbanner_id":"1038460386795368_1038461850128555",
+			"adfbinterstial_id":"1038460386795368_1038462013461872",
+			"adfbnative_id":"1038460386795368_1038462630128477",
+			"adfbnative_banner_id":"1038460386795368_1038462790128461",		
+			"adfbrectangle_banner_id":"1038460386795368_1038462413461832"
 			}
 		]
 }
